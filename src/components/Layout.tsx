88e54fa81da69d6aeea10c54219a345f99c09661
Christian Fuchs <christian.fuchs@iseag.ch>
2020-06-09 06:33:53 +0200

    initial commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,61 @@
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import clsx from "clsx";
+import React from "react";
+import Navbar from "./Navbar";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    drawerHeader: {
+      display: "flex",
+      alignItems: "center",
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: "flex-end",
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      marginLeft: -drawerWidth,
+    },
+    contentShift: {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    },
+  })
+);
+
+const Layout: React.FC = (props) => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar onDrawerChange={(change) => setOpen(change)} />
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        {props.children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/MenuDrawer.tsx
@@ -0,0 +1,109 @@
+import {
+  createStyles,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  makeStyles,
+  Theme,
+  useTheme,
+  Typography,
+} from "@material-ui/core";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import InboxIcon from "@material-ui/icons/Inbox";
+import MailIcon from "@material-ui/icons/Mail";
+import Logo from "../img/logo/foxxycodes.png";
+import React from "react";
+
+const drawerWidth = 240;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+      background: theme.palette.primary.main,
+    },
+    drawerHeader: {
+      display: "flex",
+      alignItems: "center",
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      ...theme.mixins.toolbar,
+      justifyContent: "space-between",
+    },
+    logo: {
+      width: 30,
+      borderRadius: "10px",
+      marginRight: 25,
+      marginLeft: 5,
+    },
+  })
+);
+
+export interface IMenuDrawer {
+  open: boolean;
+  onDrawerClose: () => void;
+}
+
+const MenuDrawer: React.FC<IMenuDrawer> = (props) => {
+  const { onDrawerClose, open } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <Drawer
+      className={classes.drawer}
+      variant="persistent"
+      anchor="left"
+      open={open}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+    >
+      <div className={classes.drawerHeader}>
+        <div style={{ display: "flex" }}>
+          <img src={Logo} alt="Logo" className={classes.logo} />
+          <Typography variant="subtitle1">foxxycodes</Typography>
+        </div>
+        <IconButton onClick={onDrawerClose}>
+          {theme.direction === "ltr" ? (
+            <ChevronLeftIcon />
+          ) : (
+            <ChevronRightIcon />
+          )}
+        </IconButton>
+      </div>
+      <Divider />
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+export default MenuDrawer;
